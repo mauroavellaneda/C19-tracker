0835c3b9528c80528102463a9532059d253618e6
Mauro Avellaneda <mauroeavellaneda@gmail.com>
2021-04-20 17:19:07 +0200

    Removes Mui typography

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Typography, Grid } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
 import CardComponent from "./Card/Card";
 import styles from "./Cards.module.css";
 
